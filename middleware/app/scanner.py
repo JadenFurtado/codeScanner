4ad7eb7454952f314dbb3c6a42cfc69a48a69c69
JadenFurtado <52862591+JadenFurtado@users.noreply.github.com>
2022-07-13 12:23:39 +0530

    started working on the scanner

--- middleware/app/scanner.py
+++ middleware/app/scanner.py
@@ -11,11 +11,27 @@ import docker
 # the analysis page
 @app.route("/scanner")
 def scannerOptions():
-    pass
+    scan_name = request.args.get("scan_name")
+    working_directory = request.args.get("working_directory")
+    if working_directory!=None or scan_name!=None:
+        print(working_directory[0])
+        cmd = "docker run --name "+scan_name+" -v "+str(working_directory)+":/project ort --info scan -i /project/analyzer-result.yml -o /scan"
+        cmdList = cmd.split()
+        try:
+            p = subprocess.Popen(cmdList, cwd=working_directory,stdout=PIPE)
+            p.wait()
+            text = p.communicate()[0]
+            print(text)
+            return "success"
+        except:
+            return "an exception occurred"
+    else:
+        return "working directory and scan name can't be null!"
 
-@app.route("/startScan")
+
+@app.route("/start")
 def startScan():
-    pass
+    return render_template("scannerOptions.html")
 
 @app.route("/listScans")
 def listScans():
